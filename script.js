78ec956bea2b68439c2807d645fe63697fbe1e02
teja7569 <tejavasam21@gmail.com>
2025-12-27 09:40:11 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simple smooth scrolling for navigation
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
